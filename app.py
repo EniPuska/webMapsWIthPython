069e1ea17f754ad7059306057e84d7a95403a5a9
EniPuska <eni.puska@gmail.com>
2020-06-23 13:50:34 +0300

    ....

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import folium
 import pandas
 
-map = folium.Map(location= [80, -100])
+map = folium.Map(location= [80, -100], zoom_start=6, tiles="Mapbox Bright")
 '''data = pandas.read_csv("Volcanoes.txt")
 latitude = list(data("LON"))
 longitude = list(data("LAT"))'''
