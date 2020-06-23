70a36733cb8a20c12498ed5f1abd1baeec7f30ce
EniPuska <eni.puska@gmail.com>
2020-06-23 14:03:26 +0300

    ...

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import folium
 import pandas
 
-map = folium.Map(location= [80, -100], zoom_start=6, tiles="Mapbox Bright")
+map = folium.Map(location=[37.963792, 23.722097], zoom_start=18)
+featureGroup = folium.FeatureGroup(name = "My Map")
+featureGroup.add_child(folium.Marker(location=[37.963792, 23.722097], popup="I m a Marker", icon=folium.Icon(color='green')))
+map.add_child(featureGroup)
+map.save("map.html")
 '''data = pandas.read_csv("Volcanoes.txt")
 latitude = list(data("LON"))
 longitude = list(data("LAT"))'''
