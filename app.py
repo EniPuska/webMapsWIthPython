f71b2c411b426adaf3aef33804263800a232e9c9
EniPuska <eni.puska@gmail.com>
2020-06-23 13:11:55 +0300

    ...

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import folium
 import pandas
 
 map = folium.Map(location= [80, -100])
-map.save("map.html")
 '''data = pandas.read_csv("Volcanoes.txt")
 latitude = list(data("LON"))
 longitude = list(data("LAT"))'''
